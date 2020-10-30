6b991ce5114249116f36f5b67da5b95f127ae7f0
qmb <qumingbo@foxmail.com>
2020-10-30 18:21:08 +0800

    11

--- src/main/java/com/qumingbo/test.java
+++ src/main/java/com/qumingbo/test.java
@@ -23,8 +23,25 @@ public class test {
         // String s3 = "asddsa";
         // Boolean aBoolean = ifReverse(s3);
         // System.out.println(aBoolean);
-        int[] a = {3,2,2,3};
-        System.out.println(removeElement(a,3));
+        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(diagonalSum(a));
+
+        ListNode listNode = new ListNode(1);
+        ListNode listNode2 = new ListNode(0);
+        ListNode listNode3 = new ListNode(1);
+        listNode.next = listNode2;
+        listNode2.next = listNode3;
+        System.out.println(getDecimalValue(listNode));
+
+        System.out.println(":::::::::::::::::::::::::");
+        int[] start = {1, 2, 3};
+        int[] end = {3, 2, 7};
+        int queryTime = 4;
+        System.out.println(busyStudent(start, end, queryTime));
+        System.out.println("--------------------------");
+
+        int[][] b = {{1,1}};
+        System.out.println(islandPerimeter(b));
     }
 
     /**
@@ -79,7 +96,13 @@ public class test {
         return true;
     }
 
-
+    /**
+     * 快速排序
+     *
+     * @param a      排序的数组
+     * @param low    起始位置
+     * @param height 结束位置
+     */
     public static void quickSort(int[] a, int low, int height) {
         int i, j, t, temp;
         if (low > height) {
@@ -149,11 +172,147 @@ public class test {
 
     /**
      * leetcode 27
+     *
      * @param nums
      * @param val
      * @return
      */
     public static int removeElement(int[] nums, int val) {
         return (int) Arrays.stream(nums).filter(e -> e != val).count();
+    }
+
+    /**
+     * leetcode 1295
+     *
+     * @param nums 数组
+     * @return 偶数位个数
+     */
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (String.valueOf(nums[i]).length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * leetcode 1572
+     *
+     * @param mat 二维数组
+     * @return 和
+     */
+    public static int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int length = mat.length;
+        for (int i = 0, j = length - 1; i < length; i++, j--) {
+            sum += (mat[i][i] + mat[i][j]);
+        }
+        if (length % 2 == 0) {
+            return sum;
+        } else {
+            return sum - mat[(length - 1) / 2][(length - 1) / 2];
+        }
+    }
+
+    /**
+     * 从链表中获取二进制数  并转为10进制
+     * leetcode 1290
+     *
+     * @param head
+     * @return
+     */
+    public static int getDecimalValue(ListNode head) {
+        int result = 0;
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            result += list.get(i) * Math.pow(2, count);
+            count++;
+        }
+        return result;
+    }
+
+    /**
+     * leetcode 1450
+     *
+     * @param startTime 开始时间数组
+     * @param endTime   结束时间数组
+     * @param queryTime 完成时间
+     * @return 学生个数
+     */
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for (int i = 0, j = 0; i < startTime.length && j < endTime.length; i++, j++) {
+            if (startTime[i] <= queryTime && endTime[j] >= queryTime) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * leetcode LCP 17.速算机器人
+     *
+     * @param s
+     * @return
+     */
+    public static int calculate(String s) {
+        int x = 1, y = 0;
+        String[] split = s.split("");
+        for (String s1 : split) {
+            if (s1.equals("A")) {
+                x = 2 * x + y;
+            } else if (s1.equals("B")) {
+                y = 2 * y + x;
+            }
+        }
+        return x + y;
+    }
+
+    /**
+     * leetcode 463.岛屿的周长
+     *
+     * @param grid 二维数组代表一块地方
+     * @return 岛屿的周长
+     */
+    public static int islandPerimeter(int[][] grid) {
+        int count = 0;
+        int nearby = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    count += 1;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        nearby += 1;
+                    }
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+                        nearby += 1;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        nearby += 1;
+                    }
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
+                        nearby += 1;
+                    }
+                }
+            }
+        }
+
+        return count * 4 - nearby;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
     }
 }
